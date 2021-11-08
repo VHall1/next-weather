aeb0116c571c2fd1401e0d439e216ea5e2ed2955
VHall1 <victorhallpsn@hotmail.com>
2021-11-07 23:29:21 -0300

    Create proxy request between client and API

---
+++ pages/api/weather.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// This acts as a Proxy between the client and the API
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const location = req.query?.loc;
+  if (!location) return res.status(400).send("Bad Request");
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${req.query.loc}&appid=${process.env.OPENWEATHER_KEY}`
+  )
+    .then((res) => res.json())
+    .then((weather) => res.status(200).json(weather))
+    .catch((e) => {
+      console.error(e.message);
+      return res.status(500).send("Internal Server Error");
+    });
+}
