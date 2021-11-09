35c151104add9ad92e715214c121caaded3fac6d
VHall1 <victorhallpsn@hotmail.com>
2021-11-08 21:58:50 -0300

    actually return promise from API

--- pages/api/weather.ts
+++ pages/api/weather.ts
@@ -5,13 +5,13 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const location = req.query?.loc;
   if (!location) return res.status(400).send("Bad Request");
 
-  fetch(
+  return fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${req.query.loc}&appid=${process.env.OPENWEATHER_KEY}`
   )
     .then((res) => res.json())
     .then((weather) => res.status(200).json(weather))
     .catch((e) => {
-      console.error(e.message);
+      console.warn(`Had an error fetching weather data: ${e.message}`);
       return res.status(500).send("Internal Server Error");
     });
 }
